c7e4951edfedd28751147c4fd81918b23d5ccf68
IND }JAVA{ SEC <74123134+FarhanXCode7@users.noreply.github.com>
2021-01-16 18:18:41 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,26 @@
+const donasi = (Ig, name) => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DONASI SEIKHLASNYA:)* ❉⊰━━✿
+┃  
+┣━⊱ *OVO*
+┣⊱ 08311800241
+┣━⊱ *PULSA*
+┣⊱ 08311800241
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY ${name}*
+┗━━━━━━━━━━━━━━━━━━━━
+Note:
+Jika Gamau Donasi Setidaknya Follow IG Ngab:D
+Kalo Dah Donasi Silahkan Invit Di Group Kalian:)
+Makasih:)
+
+${Ig}
+
+`
+}
+
+exports.donasi = donasi
