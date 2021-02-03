31911c1cf4491f98eab4f869e7dc75980771da9e
FarhanXCode7 <74123134+Fxc7@users.noreply.github.com>
2021-02-03 23:54:46 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm i imgbb-uploader
+npm i -g ytdl
+npm i -g cwebp
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
 echo "Updates : fix Bugs"
